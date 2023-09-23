e45d215385e40a6d53cd25987b17f9353c665601
Yajiit <tjagerter@gmail.com>
2023-09-23 10:50:33 -0400

    added components

---
+++ portfolio-react/src/components/Footer.js
@@ -0,0 +1,20 @@
+// src/componentsFooter.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <div className="social-links">
+        <a href="your-github-link" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+        <a href="your-linkedin-link" target="_blank" rel="noopener noreferrer">
+          LinkedIn
+        </a>
+        {/* Add more social links as needed */}
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ portfolio-react/src/components/Project.js
@@ -0,0 +1,21 @@
+// src/components/Project.js
+import React from 'react';
+
+function Project({ title, imageSrc, deployedLink, githubLink }) {
+  return (
+    <div className="project-card">
+      <h3>{title}</h3>
+      <img src={imageSrc} alt={`${title} screenshot`} />
+      <div className="project-links">
+        <a href={deployedLink} target="_blank" rel="noopener noreferrer">
+          Live Demo
+        </a>
+        <a href={githubLink} target="_blank" rel="noopener noreferrer">
+          GitHub Repo
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Project;
