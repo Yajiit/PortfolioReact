0aa0f9b7f2c80e51453c792d22612269319921cc
Yajiit <tjagerter@gmail.com>
2023-09-24 14:44:59 -0400

    Footer now displays properly

--- portfolio-react/src/App.jsx
+++ portfolio-react/src/App.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import AboutMe from './pages/AboutMe';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
@@ -18,6 +19,7 @@ function App() {
         <Route path="/" element={<AboutMe />} />
         <Route path="/resume" element={<Resume />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
--- portfolio-react/src/components/Footer.jsx
+++ portfolio-react/src/components/Footer.jsx
@@ -5,10 +5,10 @@ function Footer() {
   return (
     <footer>
       <div className="social-links">
-        <a href="your-github-link" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/Yajiit/" target="_blank" rel="noopener noreferrer">
           GitHub
         </a>
-        <a href="your-linkedin-link" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/timothy-agerter-491187293/" target="_blank" rel="noopener noreferrer">
           LinkedIn
         </a>
         {/* Add more social links as needed */}
