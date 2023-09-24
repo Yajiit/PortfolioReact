1ba15149ccbccc28998ef6141e1b8e4392a5a7d5
Yajiit <tjagerter@gmail.com>
2023-09-24 02:27:12 -0400

    added projectData to Portfolio page

--- portfolio-react/src/components/Project.jsx
+++ portfolio-react/src/components/Project.jsx
@@ -1,13 +1,13 @@
 // src/components/Project.js
 import React from 'react';
 
-function Project({ title, imageSrc, deployedLink, githubLink }) {
+function Project({ title, imageSrc, demoLink, githubLink }) {
   return (
     <div className="project-card">
       <h3>{title}</h3>
       <img src={imageSrc} alt={`${title} screenshot`} />
       <div className="project-links">
-        <a href={deployedLink} target="_blank" rel="noopener noreferrer">
+        <a href={demoLink} target="_blank" rel="noopener noreferrer">
           Live Demo
         </a>
         <a href={githubLink} target="_blank" rel="noopener noreferrer">
--- portfolio-react/src/pages/Portfolio.jsx
+++ portfolio-react/src/pages/Portfolio.jsx
@@ -1,22 +1,22 @@
 // src/pages/Portfolio.jsx
 import React from 'react';
 import Project from '../components/Project'; // Import Project component
-// import projectData from '../utils/projectData'; // Import project data
+import projectData from '../utils/projectData'; // Import project data
 
 function Portfolio() {
   return (
     <section>
       <h2>Portfolio</h2>
       <div className="project-list">
-        {/* {projectData.map((project, index) => (
+        {projectData.map((project, index) => (
           <Project
             key={index}
             title={project.title}
             imageSrc={project.imageSrc}
-            deployedLink={project.deployedLink}
+            demoLink={project.demoLink}
             githubLink={project.githubLink}
           />
-        ))} */}
+        ))}
       </div>
     </section>
   );
